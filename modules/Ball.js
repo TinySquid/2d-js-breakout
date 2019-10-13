a855f94d82f6d4ab7c2240b34e1e1930ba897662
Michael Nunes <miketnunes@gmail.com>
2019-10-13 16:20:58 -0700

    Refactor ball component

--- modules/Ball.js
+++ modules/Ball.js
@@ -1,8 +1,8 @@
 import { ctx } from './Canvas.js'
 
 export default class Ball {
-  constructor(x, y, size, color, speed) {
-    this.position = { x: x, y: y };
+  constructor(position, size, color, speed) {
+    this.position = position;
     this.size = size;
     this.color = color;
     this.speed = speed;
@@ -15,48 +15,5 @@ export default class Ball {
     ctx.fillStyle = this.color;
     ctx.fill();
     ctx.closePath();
-  }
-
-  getPos() {
-    return this.position;
-  }
-
-  setPos(position) {
-    //New position object must only have two keys
-    if (Object.keys(position).length === 2) {
-      //Keys MUST be only x & y
-      if (position.hasOwnProperty('x') && position.hasOwnProperty('y')) {
-        this.position = position;
-      } else {
-        console.error(`Error: setPos expects format to be: '{x: value, y: value}'.`);
-      }
-    } else {
-      console.error(`Error: setPos expects an object that has two keys (x, y).`);
-    }
-    return true;
-  }
-
-  getSpeed() {
-    return this.speed;
-  }
-
-  setSpeed(speed) {
-    this.speed = Number(speed);
-  }
-
-  getSize() {
-    return this.size;
-  }
-
-  setSize(size) {
-    this.size = Number(size);
-  }
-
-  // getColor(){
-  //   return this.color;
-  // }
-
-  setColor(color) {
-    this.color = color;
   }
 }
